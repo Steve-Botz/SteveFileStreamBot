c0d2bad07b7c3b2ff71a703546b78c204b4b29c5
Aman vishwakarma <151424799+Botsthe@users.noreply.github.com>
2025-07-29 10:59:43 +0530

    AVBOTz v4.6.00
    Signed-off-by: Aman vishwakarma  <151424799+Botsthe@users.noreply.github.com>

--- web/utils/config_parser.py
+++ web/utils/config_parser.py
@@ -37,3 +37,7 @@ class TokenParser:
             if key.startswith("MULTI_TOKEN") and value.strip() != ""
         }
         return self.tokens
+
+# Dont Remove My Credit @AV_BOTz_UPDATE 
+# This Repo Is By @BOT_OWNER26 
+# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
